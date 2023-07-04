1da7b9e9868b0a516aae6cd1b5f474c91e2b90ed
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-04 20:22:11 +0530

    Added:Uc6 -  Use Random to determine Heads or Tails and assign accordingly who starts first, the computer or the user

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package tic_tac_toe;
 public class Main {
     public static void main(String[] args) {
         Tic_tac_Toe_Game game = new Tic_tac_Toe_Game();
+        game.toss();
         game.playGame();
     }
 }
--- Tic_tac_Toe_Game.java
+++ Tic_tac_Toe_Game.java
@@ -1,5 +1,6 @@
 package tic_tac_toe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Tic_tac_Toe_Game {
@@ -20,20 +21,36 @@ public class Tic_tac_Toe_Game {
         }
     }
 
-    // create method playgame
+    /* Uc 6: - Use Random to determine Heads or Tails and assign accordingly
+     who starts first, the player 1 or the player 2.*/
+    public void toss() {
+        Random random = new Random();
+        int coinToss = random.nextInt(2);
+        boolean userInput = (coinToss == 0);
+        if (userInput) {
+            System.out.println("Player 1 won the coin toss! Player 1 play first");
+            currentplayer = 'x';
+        } else {
+            System.out.println("Player 2 won the coin toss! Player 2 play first");
+            currentplayer = 'o';
+        }
+    }
+
+    // create method play game
     public void playGame() {
         boolean gameWon = false;
         boolean gameTied = false;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the tic tac toe game ");
         System.out.println("Player 1 : X");
-        System.out.println("Player 1 : O");
+        System.out.println("Player 2 : O");
         System.out.println("start the game");
 
         //uc4
         while (!gameWon && !gameTied) {
             System.out.println("current board");
             printBoard();
+
             System.out.println("player" + currentplayer + "enter your move(row[1-9] and column [1-9] :");
             int row = scanner.nextInt();
             int col = scanner.nextInt();
@@ -43,30 +60,35 @@ public class Tic_tac_Toe_Game {
             } else {
                 System.out.println("Invalid Move");
             }
+            System.out.println("Final board:");
+            printBoard();
+            if (currentplayer == 'x') {
+                currentplayer = 'o';
+            } else {
+                currentplayer = 'x';
+            }
         }
-            scanner.close();
-        }
-
+        scanner.close();
+    }
 
     // valid till these condition
     private boolean isValidMove(int row, int col) {
-        if(row <1 || row >9 || col<1 || col>9){
+        if (row < 1 || row > 9 || col < 1 || col > 9) {
             return false;
         }
-        return board[row][col]=='-';
+        return board[row][col] == '-';
     }
-       //UC3 - method show Board to display the current Board
-        private void printBoard() {
-        for (int row = 1; row < 10; row++)
-        {
+
+    //UC3 - method show Board to display the current Board
+    private void printBoard() {
+        for (int row = 1; row < 10; row++) {
             for (int col = 1; col < 10; col++) {
-            System.out.print(board[row][col] + " ");
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
         }
         System.out.println();
     }
-        System.out.println();
-    }
-
 }
 
 
