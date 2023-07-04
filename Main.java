5c1d4bba1adfee166d294d6f9f1097e4cf92d357
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-04 16:43:25 +0530

    Added: UC4

---
+++ Main.java
@@ -0,0 +1,8 @@
+package tic_tac_toe;
+
+public class Main {
+    public static void main(String[] args) {
+        Tic_tac_Toe_Game game = new Tic_tac_Toe_Game();
+        game.playGame();
+    }
+}
--- Tic_tac_Toe_Game.java
+++ Tic_tac_Toe_Game.java
@@ -1,22 +1,68 @@
 package tic_tac_toe;
 
+import java.util.Scanner;
+
 public class Tic_tac_Toe_Game {
-    public class TicTacToe{
-        private char[][] board ;
-        private char currentplayer;
+    private char[][] board;
+    private char currentplayer;
 
-        public TicTacToe() {
-          board = new char[10][10];
-          currentplayer ='x';
-          initalizeBoard();
-        }
+    public Tic_tac_Toe_Game() {
+        board = new char[10][10];
+        currentplayer = 'x';
+        initalizeBoard();
+    }
 
-        private void initalizeBoard(){
-            for(int row = 1 ; row <=10; row++) {
-                for (int col = 1; col < 10; col++) {
-                    board[row][col] = '-';
-                }
+    private void initalizeBoard() {
+        for (int row = 1; row < 10; row++) {
+            for (int col = 1; col < 10; col++) {
+                board[row][col] = '-';
             }
         }
     }
+
+    // create method
+    public void playGame() {
+        boolean gameWon = false;
+        boolean gameTied = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the tic tac toe game ");
+        System.out.println("Player 1 : X");
+        System.out.println("Player 1 : O");
+        System.out.println("start the game");
+
+        //uc4
+        while (!gameWon && gameTied) {
+            System.out.println("current board");
+            printBoard();
+            System.out.println("player"+ currentplayer+ "enter your move(row[1-9] and column [1-9] :");
+            int row = scanner.nextInt();
+            int col = scanner.nextInt();
+
+            if(isValidMove(row,col)){
+                board[row][col] = currentplayer;
+
+        }
+            scanner.close();
+        }
+    }
+    //UC3
+        private void printBoard() {
+        for (int row = 1; row < 10; row++)
+        {
+            for (int col = 1; col < 10; col++) {
+            System.out.print(board[row][col] + " ");
+        }
+        System.out.println();
+    }
+        System.out.println();
+    }
+// valid till these condition
+    private boolean isValidMove(int row, int col) {
+        if(row <1 || row >9 || col<1 || col>9){
+            return false;
+        }
+        return board[row][col]=='-';
+    }
 }
+
+
