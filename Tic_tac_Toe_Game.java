9b59f472613c283ef5d5c4f9fd055f30e229e44f
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-04 16:21:50 +0530

    Added: UC3_Display the current board

--- Tic_tac_Toe_Game.java
+++ Tic_tac_Toe_Game.java
@@ -3,33 +3,48 @@ package tic_tac_toe;
 import java.util.Scanner;
 
 public class Tic_tac_Toe_Game {
-        private char[][] board ;
-        private char currentplayer;
+    private char[][] board;
+    private char currentplayer;
 
-        public Tic_tac_Toe_Game() {
-          board = new char[10][10];
-          currentplayer ='x';
-          initalizeBoard();
-        }
+    public Tic_tac_Toe_Game() {
+        board = new char[10][10];
+        currentplayer = 'x';
+        initalizeBoard();
+    }
 
-        private void initalizeBoard(){
-            for(int row = 1 ; row <=10; row++) {
-                for (int col = 1; col < 10; col++) {
-                    board[row][col] = '-';
-                }
+    private void initalizeBoard() {
+        for (int row = 1; row < 10; row++) {
+            for (int col = 1; col < 10; col++) {
+                board[row][col] = '-';
             }
         }
-        // create method
-        public void playGame(){
-            boolean gameWon = false;
-            boolean fameTied = false;
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Welcome to the tic tac toe game ");
-            System.out.println("Player 1 : X");
-            System.out.println("Player 1 : O");
-            System.out.println("start the game");
-            scanner.close();
-
-        }
     }
+
+    // create method
+    public void playGame() {
+        boolean gameWon = false;
+        boolean fameTied = false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the tic tac toe game ");
+        System.out.println("Player 1 : X");
+        System.out.println("Player 1 : O");
+        System.out.println("start the game");
+
+        System.out.println("current board");
+        printBoard();
+        scanner.close();
+
+    }
+
+    //UC3
+    private void printBoard() {
+        for (int row = 1; row < 10; row++) {
+            for (int col = 1; col < 10; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
 
