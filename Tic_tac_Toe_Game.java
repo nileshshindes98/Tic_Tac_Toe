09c379e016ab2e07dd60fcca6253ecf7a5cea78c
Nilesh Shinde <nileshshindes98@gmail.com>
2023-07-04 15:46:32 +0530

    Added:Tictactoe game class

---
+++ Tic_tac_Toe_Game.java
@@ -0,0 +1,22 @@
+package tic_tac_toe;
+
+public class Tic_tac_Toe_Game {
+    public class TicTacToe{
+        private char[][] board ;
+        private char currentplayer;
+
+        public TicTacToe() {
+          board = new char[10][10];
+          currentplayer ='x';
+          initalizeBoard();
+        }
+
+        private void initalizeBoard(){
+            for(int row = 1 ; row <=10; row++){
+                for(int col =1;col<10; col++){
+                    board[row][col] = '-';
+                }
+            }
+        }
+    }
+}
